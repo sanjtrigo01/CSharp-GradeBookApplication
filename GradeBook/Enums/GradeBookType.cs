cdf626c2cf5f7e5d2621b0f8fac048f0642599de
Sanj Vij <sanjay.vij01@gmail.com>
2018-08-05 18:23:56 +0100

    new grade book types

---
+++ GradeBook/Enums/GradeBookType.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace GradeBook.Enums
+{
+
+    //Create a new Enum GradeBookType containing the types "Standard", "Ranked", "ESNU", "OneToFour", and "SixPoint".
+    public enum GradleBookType
+    {
+        Standard,
+        Ranked,
+        ESNU,
+        OneToFour,
+        SixPoint
+    }
+}
